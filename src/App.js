1fef94c3c8081b6e71e8221d86226a20104a99b4
Rishabh <rishabhvicky98@gmail.com>
2022-08-19 18:58:16 +0530

    yo

--- src/App.js
+++ src/App.js
@@ -1,15 +1,8 @@
 import React, { Component } from 'react'
-import Navbar from './Navbar'
-import News from './News'
-import {
-  BrowserRouter,
-  Routes,
-  Route
-} from "react-router-dom";
 
 
 export default class App extends Component {
-  pagesize=15;
+
   render() {
     return (
         
