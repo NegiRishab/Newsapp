0d7d280912906053f5e1a0ff1270a9d63886c5d9
Rishabh <rishabhvicky98@gmail.com>
2022-08-19 18:07:12 +0530

    remove api

---
+++ src/Navbar.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import {Link} from "react-router-dom";
+
+
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <div>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+hello world
+</nav>
+      </div>
+    )
+  }
+}
